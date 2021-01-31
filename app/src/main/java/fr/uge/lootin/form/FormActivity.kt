6a425457e6d8c4e48a77376628905a0a7447f9f2
LouisBillaut <louisbillaut2000@gmail.com>
2021-01-31 15:20:31 +0100

    feat: First form page added respecting design model

--- app/src/main/java/fr/uge/lootin/form/FormActivity.kt
+++ app/src/main/java/fr/uge/lootin/form/FormActivity.kt
@@ -1,7 +1,9 @@
 package fr.uge.lootin.form
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,13 +23,23 @@ class FormActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form)
+        setContentView(R.layout.pseudo)
 
+        findViewById<Button>(R.id.nextButton).setOnClickListener {
+            if (findViewById<EditText>(R.id.pseudoText).text.toString() == "") Toast.makeText(
+                applicationContext,
+                applicationContext.getString(R.string.pseudoMessageError),
+                Toast.LENGTH_SHORT
+            ).show()
+            else setContentView(R.layout.activity_form)
+        }
+
+        /*
         Log.i("test", Game.loadCards(this, GAMES_PATH).toString())
 
         gameRV = findViewById(R.id.gameRecyclerView)
         gameAdapter = GameAdapter(Game.loadCards(this, GAMES_PATH)!!)
         gameRV.adapter = gameAdapter
-        gameRV.layoutManager = createLayoutManager()
+        gameRV.layoutManager = createLayoutManager()*/
     }
 }
