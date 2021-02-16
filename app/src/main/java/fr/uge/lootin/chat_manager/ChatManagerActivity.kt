8e672a80d138d981186325eff962fba421e560a4
Chicaa <croharejeanne@gmail.com>
2021-02-16 21:02:16 +0100

    feat: first db requests

--- app/src/main/java/fr/uge/lootin/chat_manager/ChatManagerActivity.kt
+++ app/src/main/java/fr/uge/lootin/chat_manager/ChatManagerActivity.kt
@@ -1,17 +1,122 @@
 package fr.uge.lootin.chat_manager
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Point
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import fr.uge.lootin.R
-import fr.uge.lootin.chat_manager.model.MyRenderer
-import fr.uge.lootin.chat_manager.model.Phone
-import fr.uge.lootin.chat_manager.model.PhoneCategory
+import org.json.JSONObject
+import kotlin.jvm.Throws
+
+
 //import iammert.com.expandablelib.ExpandableLayout
 //import iammert.com.expandablelib.Section
 
 class ChatManagerActivity : AppCompatActivity() {
+    /*
+
+    //var url = "http://localhost:8080/login"
+        val url = "http://192.168.43.2:8080/login"
+        //var url = "http://192.168.43.2:80/showLogin"
+
+        Log.i("my_log", "encore avant")
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url, JSONObject("{\"username\": \"Loulou\",\"password\": \"Yvette\"}"),
+                Response.Listener { response ->
+                    Log.i("my_log", "Response: %s".format(response.toString()))
+                },
+                Response.ErrorListener { error ->
+                    Log.i("my_log", "errooooooooooooooooooooooooooooooorrrrrrr!!!!!!!!!!!!!!!!!!!!!\n"
+                            + error.toString() + "\n"
+                            + error.networkResponse + "\n"
+                            + error.localizedMessage + "\n"
+                            + error.message + "\n"
+                            + error.cause + "\n"
+                            + error.stackTrace.toString())
+                }
+        )
+        /*
+        val stringRequest: StringRequest =
+                StringRequest(
+                        Request.Method.GET, url,
+                        Response.Listener { response ->
+
+                                Log.i("my_log", "c'est bon " + response.toString())
+                                //findViewById<TextView>(R.id.textResult).text = "yes"
+
+
+                        },
+                        Response.ErrorListener { error ->
+                            Log.i("my_log", "errooooooooooooooooooooooooooooooorrrrrrr!!!!!!!!!!!!!!!!!!!!!\n" + error.toString() + "\n" + error.networkResponse)
+                        })*/
+
+     */
+
+    private fun connect(queue: RequestQueue): String{
+        val url = "http://192.168.43.2:8080/login"
+        Log.i("my_log", "connect request")
+        var token: String = ""
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url, JSONObject("{\"username\": \"Loulou\",\"password\": \"Yvette\"}"),
+                Response.Listener { response ->
+                    Log.i("my_log", "Connect Response: %s".format(response.toString()));
+                    val jsonResponse = JSONObject(response.toString());
+                    token = jsonResponse.getString("jwt")
+
+                },
+                Response.ErrorListener { error ->
+                    Log.i("my_log", "error while trying to connect\n"
+                            + error.toString() + "\n"
+                            + error.networkResponse + "\n"
+                            + error.localizedMessage + "\n"
+                            + error.message + "\n"
+                            + error.cause + "\n"
+                            + error.stackTrace.toString())
+                }
+        )
+        queue.add(jsonObjectRequest)
+        return token
+    }
+
+    private fun verifyConnect(queue: RequestQueue, token: String){
+        val url = "http://192.168.43.2:8080/showLogin"
+
+        Log.i("my_log", "verify connexion request")
+        val stringRequest = object : StringRequest(Request.Method.GET, url,
+                Response.Listener { response ->
+                    Log.i("my_log", "Response: %s".format(response))
+                },
+                Response.ErrorListener { error ->
+                    Log.i("my_log", "error while trying to verify connexion\n"
+                            + error.toString() + "\n"
+                            + error.networkResponse + "\n"
+                            + error.localizedMessage + "\n"
+                            + error.message + "\n"
+                            + error.cause + "\n"
+                            + error.stackTrace.toString())
+                }
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val params: MutableMap<String, String> = HashMap()
+                Log.i("my_log", token);
+                params["Authorization"] = "Bearer " + token
+                return params
+            }
+        }
+
+        queue.add(stringRequest)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_manager)
@@ -42,6 +147,33 @@ class ChatManagerActivity : AppCompatActivity() {
         var recyclerViewMatches : RecyclerView = findViewById(R.id.matchRecyclerView)
         recyclerViewMatches.adapter = matchesAdapter
         recyclerViewMatches.layoutManager = GridLayoutManager(this, 1, RecyclerView.HORIZONTAL, false)
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val height = displayMetrics.heightPixels
+        val width = displayMetrics.widthPixels
+        Log.i("my_log", "deprecated method: " + height + "     " + width)
+
+
+        var chatManager : ConstraintLayout =findViewById(R.id.chat_manager_id)
+
+        Log.i("my_log", "chatManager.height and width: " +  chatManager.height + "     " + chatManager.width)
+        Log.i("my_log", "chatManager.measuredHeight and measuredWidth: " +  chatManager.measuredWidth + "     " + chatManager.measuredHeight)
+        Log.i("my_log", "chatManager.bacckgroundColor " +  chatManager.background)
+
+        val display = windowManager.defaultDisplay
+        val size = Point()
+        display.getSize(size)
+        val width2: Int = size.x
+        val height2: Int = size.y
+        Log.i("my_log", "deprecated method 2 : " + height2 + "     " + width2)
+        Log.i("my_log", "deprecated method 2 : " + height2 + "     " + width2)
+        Log.i("my_log", "deprecated method 2 : " + height2 + "     " + width2)
+
+        val queue = Volley.newRequestQueue(this)
+        val token = connect(queue)
+        verifyConnect(queue, token)
 
     }
 
