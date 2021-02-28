bb5738c85db8da38eb61df76f8e07f8323002354
JUILLARD <tjuillar@etud.u-pem.fr>
2021-02-28 15:52:18 +0100

    add : first version front web socket

--- app/src/main/java/fr/uge/lootin/chat/ChatActivity.kt
+++ app/src/main/java/fr/uge/lootin/chat/ChatActivity.kt
@@ -11,11 +11,97 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import fr.uge.lootin.R
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
+import java.net.URI
 import java.util.*
 
 class ChatActivity : AppCompatActivity() {
     lateinit var recycler : RecyclerView
     lateinit var adapter: ChatAdapter
+    private lateinit var webSocketClient: WebSocketClient
+
+    override fun onResume() {
+        super.onResume()
+        initWebSocket()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        webSocketClient.close()
+    }
+
+    private fun initWebSocket() {
+        val coinbaseUri: URI? = URI(WEB_SOCKET_URL)
+
+        createWebSocketClient(coinbaseUri)
+    }
+
+    private fun createWebSocketClient(coinbaseUri: URI?) {
+        webSocketClient = object : WebSocketClient(coinbaseUri) {
+
+            override fun onOpen(handshakedata: ServerHandshake?) {
+                Log.d(TAG, "onOpen")
+                subscribe()
+            }
+
+            override fun onMessage(message: String?) {
+                Log.d(TAG, "onMessage: $message")
+                setUpBtcPriceText(message)
+            }
+
+            override fun onClose(code: Int, reason: String?, remote: Boolean) {
+                Log.d(TAG, "onClose")
+                unsubscribe()
+            }
+
+            override fun onError(ex: Exception?) {
+                Log.e("createWebSocketClient", "onError: ${ex?.message}")
+            }
+        }
+    }
+
+    /**
+     * TODO
+     */
+    private fun subscribe() {
+        webSocketClient.send(
+                "{\n" +
+                        "    \"type\": \"subscribe\",\n" +
+                        "    \"channels\": [{ \"name\": \"ticker\", \"product_ids\": [\"BTC-EUR\"] }]\n" +
+                        "}"
+        )
+    }
+
+    /**
+     * TODO adapter
+     */
+    private fun setUpBtcPriceText(message: String?) {
+        message?.let {
+            /*
+            val moshi = Moshi.Builder().build()
+            val adapter: JsonAdapter<BitcoinTicker> = moshi.adapter(BitcoinTicker::class.java)
+            val bitcoin = .fromJson(message)
+            runOnUiThread { btc_price_tv.text = "1 BTC: ${bitcoin?.price} €" }
+
+             */
+        }
+    }
+
+    /**
+     * TODO
+     */
+    private fun unsubscribe() {
+        webSocketClient.send(
+                "{\n" +
+                        "    \"type\": \"unsubscribe\",\n" +
+                        "    \"channels\": [\"ticker\"]\n" +
+                        "}"
+        )
+    }
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -72,5 +158,11 @@ class ChatActivity : AppCompatActivity() {
         Toast.makeText(this, "Send vocal is not implemented yet", Toast.LENGTH_LONG).show()
     }
 
+
+
+    companion object {
+        const val WEB_SOCKET_URL = "wss://ws-feed.pro.coinbase.com"
+        const val TAG = "Coinbase"
+    }
 
 }
