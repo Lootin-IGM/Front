01c90517ed136e3e33946f97b0b8b5e3abdd9662
jcrohare <croharejeanne@gmail.com>
2021-03-11 23:29:37 +0100

    update: can scroll on messages and matches

--- app/src/main/java/fr/uge/lootin/chat_manager/ChatManagerActivity.kt
+++ app/src/main/java/fr/uge/lootin/chat_manager/ChatManagerActivity.kt
@@ -1,13 +1,8 @@
 package fr.uge.lootin.chat_manager
 
-import android.content.Context
-import android.graphics.Point
-import android.os.Build
 import android.os.Bundle
-import android.util.DisplayMetrics
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.*
@@ -17,56 +12,18 @@ import org.json.JSONException
 import org.json.JSONObject
 import java.io.UnsupportedEncodingException
 import java.nio.charset.Charset
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.ZonedDateTime
+import kotlin.math.sign
 
+const val URL = "http://192.168.43.2:8080"
 
-//import iammert.com.expandablelib.ExpandableLayout
-//import iammert.com.expandablelib.Section
+// TODO ATTENTION, NE DOIVENT PAS ETRE NULS!!!!!!!!
+const val SIZE_PAGE_MATCHES = 1
+const val SIZE_PAGE_PREVIEW_MESSAGE = 1
 
 class ChatManagerActivity : AppCompatActivity() {
-    /*
 
-    //var url = "http://localhost:8080/login"
-        val url = "http://192.168.43.2:8080/login"
-        //var url = "http://192.168.43.2:80/showLogin"
-
-        Log.i("my_log", "encore avant")
-
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url, JSONObject("{\"username\": \"Loulou\",\"password\": \"Yvette\"}"),
-                Response.Listener { response ->
-                    Log.i("my_log", "Response: %s".format(response.toString()))
-                },
-                Response.ErrorListener { error ->
-                    Log.i("my_log", "errooooooooooooooooooooooooooooooorrrrrrr!!!!!!!!!!!!!!!!!!!!!\n"
-                            + error.toString() + "\n"
-                            + error.networkResponse + "\n"
-                            + error.localizedMessage + "\n"
-                            + error.message + "\n"
-                            + error.cause + "\n"
-                            + error.stackTrace.toString())
-                }
-        )
-        /*
-        val stringRequest: StringRequest =
-                StringRequest(
-                        Request.Method.GET, url,
-                        Response.Listener { response ->
-
-                                Log.i("my_log", "c'est bon " + response.toString())
-                                //findViewById<TextView>(R.id.textResult).text = "yes"
-
-
-                        },
-                        Response.ErrorListener { error ->
-                            Log.i("my_log", "errooooooooooooooooooooooooooooooorrrrrrr!!!!!!!!!!!!!!!!!!!!!\n" + error.toString() + "\n" + error.networkResponse)
-                        })*/
-
-     */
-
-    private fun verifyConnect(queue: RequestQueue, token: String){
-        val url = "http://192.168.43.2:8080/showLogin"
+    private fun requestVerifyConnect(queue: RequestQueue, token: String){
+        val url = URL + "/showLogin"
 
         Log.i("my_log", "verify connexion request")
         val stringRequest = object : StringRequest(Request.Method.GET, url,
@@ -93,8 +50,8 @@ class ChatManagerActivity : AppCompatActivity() {
         queue.add(stringRequest)
     }
 
-    private fun getMatches(queue: RequestQueue, token: String, nb_matches: Int, page: Int, list_matches: ArrayList<Match>, list_messages: ArrayList<PreviewMessage>, matchAdapter: MatchAdapter, previewMessageAdapter: PreviewMessageAdapter) {
-        val url = "http://192.168.43.2:8080/matches"
+    private fun requestGetMatches(queue: RequestQueue, token: String, nb_matches: Int, page: Int, list_matches: ArrayList<Match>, list_messages: ArrayList<PreviewMessage>, matchAdapter: MatchAdapter, previewMessageAdapter: PreviewMessageAdapter) {
+        val url = URL + "/matches"
 
         Log.i("my_log", "get matches request")
 
@@ -164,17 +121,116 @@ class ChatManagerActivity : AppCompatActivity() {
         queue.add(jsonObjectRequest)
     }
 
+    private fun treatEmptyMatches(response: JSONObject, list_matches: ArrayList<Match>, matchAdapter: MatchAdapter) {
+        val data = response.getJSONArray("data")
+        val matchesSize = list_matches.size
+        for (i in 0 until data.length()) {
+            val match = data.getJSONObject(i)
+            // TODO A traiter quand ce sera prêt (photo + id)
+            if (match.isNull("lastMessage")) {
+                Log.i("my_log", "match?? " + match.getString("id"))
+                list_matches.add(Match())
+            }
+        }
+        matchAdapter.notifyItemInserted(matchesSize)
+    }
+
+    private fun treatLastMessage(response: JSONObject, list_messages: ArrayList<PreviewMessage>, previewMessageAdapter: PreviewMessageAdapter) {
+        val data = response.getJSONArray("data")
+        val previewMessagesSize = list_messages.size
+        for (i in 0 until data.length()) {
+            val match = data.getJSONObject(i)
+
+            // TODO Rajouter photo quand ce sera prêt
+            val lastMessage = match.getJSONObject("lastMessage")
+            list_messages.add(PreviewMessage(lastMessage.getString("message"), match.getJSONObject("user").getString("login"), (0..3).random()))
+            Log.i("my_log", "message?? " + match.getString("id"))
+
+            //Log.i("my_log",lastMessage.getString("sendTime").toString().indexOf("+").toString())
+            //Log.i("my_log",ZonedDateTime.parse(lastMessage.getString("sendTime").toString()).toString())
+            //Log.i("my_log", LocalDateTime.parse(lastMessage.getString("sendTime")).toString())
+        }
+        previewMessageAdapter.notifyItemInserted(previewMessagesSize)
+    }
+
+    private fun requestGetEmptyMatches(queue: RequestQueue, token: String, nb_matches: Int, page: Int, list_matches: ArrayList<Match>, matchAdapter: MatchAdapter) {
+        val url = URL + "/matches/empty"
+
+        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.POST, url, JSONObject("{\"nbMatches\": " + nb_matches + ",\"page\":" + page + "}"),
+                object : Response.Listener<JSONObject?>{
+                    override fun onResponse(response: JSONObject?) {
+                        if (response != null) {
+                            treatEmptyMatches(response, list_matches, matchAdapter)
+                        }
+                    }
+                },
+                Response.ErrorListener { error ->
+                    Log.i("my_log", "error while trying to get matches\n"
+                            + error.toString() + "\n"
+                            + "networkResponse " + error.networkResponse + "\n"
+                            + "localizedMessage " + error.localizedMessage + "\n"
+                            + "message " + error.message + "\n"
+                            + "cause " + error.cause + "\n"
+                            + "stackTrace " + error.stackTrace.toString())
+                }
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val params: MutableMap<String, String> = HashMap()
+                params["Authorization"] = "Bearer " + token
+                return params
+            }
+        }
+        queue.add(jsonObjectRequest)
+    }
+
+    private fun requestGetLastMessages(queue: RequestQueue, token: String, nb_matches: Int, page: Int, list_messages: ArrayList<PreviewMessage>, previewMessageAdapter: PreviewMessageAdapter) {
+        val url = URL + "/matches/lastMsg"
+
+        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.POST, url, JSONObject("{\"nbMatches\": " + nb_matches + ",\"page\":" + page + "}"),
+                object : Response.Listener<JSONObject?>{
+                    override fun onResponse(response: JSONObject?) {
+                        if (response != null) {
+                            treatLastMessage(response, list_messages, previewMessageAdapter)
+                        }
+                    }
+                },
+                Response.ErrorListener { error ->
+                    Log.i("my_log", "error while trying to get matches\n"
+                            + error.toString() + "\n"
+                            + "networkResponse " + error.networkResponse + "\n"
+                            + "localizedMessage " + error.localizedMessage + "\n"
+                            + "message " + error.message + "\n"
+                            + "cause " + error.cause + "\n"
+                            + "stackTrace " + error.stackTrace.toString())
+                }
+        ) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String>? {
+                val params: MutableMap<String, String> = HashMap()
+                params["Authorization"] = "Bearer " + token
+                return params
+            }
+        }
+        queue.add(jsonObjectRequest)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_manager)
 
-        val list_messages = ArrayList<PreviewMessage>()
+        val queue = Volley.newRequestQueue(this)
+        val tmp_token: String? = intent.getStringExtra(TOKEN_VALUE)
+        var token = ""
+        if(tmp_token != null) {
+            token = tmp_token
+        }
 
-
-        val previewMessagesAdapter = PreviewMessageAdapter(list_messages)
+        val listMessages = ArrayList<PreviewMessage>()
+        val previewMessagesAdapter = PreviewMessageAdapter(listMessages)
         var recyclerViewMessagePreview : RecyclerView = findViewById(R.id.previewMessagesId)
         recyclerViewMessagePreview.adapter = previewMessagesAdapter
-
         recyclerViewMessagePreview.layoutManager = GridLayoutManager(this, 1, RecyclerView.VERTICAL, false)
         recyclerViewMessagePreview.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
@@ -182,45 +238,42 @@ class ChatManagerActivity : AppCompatActivity() {
                 Log.i("my_log", newState.toString())
                 if (!recyclerView.canScrollVertically(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
                     /*TODO load messages*/
+                    requestGetLastMessages(queue, token, SIZE_PAGE_PREVIEW_MESSAGE, listMessages.size / SIZE_PAGE_PREVIEW_MESSAGE, listMessages, previewMessagesAdapter)
+                    /*val size = listMessages.size
+                    listMessages.add(PreviewMessage("Le refresh a fonctionné :)", "Jeanne", (0..3).random()))
+                    previewMessagesAdapter.notifyItemInserted(size)*/
 
-                    val size = list_messages.size
-                    list_messages.add(PreviewMessage("Le refresh a fonctionné :)", "Jeanne", (0..3).random()))
-                    previewMessagesAdapter.notifyItemInserted(size)
-                    Log.d("-----", "end");
                 }
             }
         })
 
-        val list_matches = ArrayList<Match>()
-        val matchesAdapter = MatchAdapter(list_matches)
+        val listMatches = ArrayList<Match>()
+        val matchesAdapter = MatchAdapter(listMatches)
         var recyclerViewMatches : RecyclerView = findViewById(R.id.matchRecyclerView)
         recyclerViewMatches.adapter = matchesAdapter
         recyclerViewMatches.layoutManager = GridLayoutManager(this, 1, RecyclerView.HORIZONTAL, false)
-
         recyclerViewMatches.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
                 Log.i("my_log", newState.toString())
                 if (!recyclerView.canScrollHorizontally(1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
                     /*TODO load matches */
+                    requestGetEmptyMatches(queue, token, SIZE_PAGE_MATCHES, listMatches.size / SIZE_PAGE_MATCHES, listMatches, matchesAdapter)
+                    /*val size = listMatches.size
+                    listMatches.add(Match())
+                    matchesAdapter.notifyItemInserted(size)*/
 
-                    val size = list_matches.size
-                    list_matches.add(Match())
-                    matchesAdapter.notifyItemInserted(size)
-                    Log.d("-----", "end");
                 }
             }
         })
 
 
+        Log.i("my_log", "dans 2nd acti  " + token)
+        requestVerifyConnect(queue, token)
+        //requestGetMatches(queue, token, 4, 0, list_matches, list_messages, matchesAdapter, previewMessagesAdapter)
+        requestGetEmptyMatches(queue, token, SIZE_PAGE_MATCHES, 0, listMatches, matchesAdapter)
+        requestGetLastMessages(queue, token, SIZE_PAGE_PREVIEW_MESSAGE, 0, listMessages, previewMessagesAdapter)
 
-        val queue = Volley.newRequestQueue(this)
-        val token: String? = intent.getStringExtra(TOKEN_VALUE)
-        if (token != null) {
-            Log.i("my_log", "dans 2nd acti  " + token)
-            verifyConnect(queue, token)
-            getMatches(queue, token, 4, 0, list_matches, list_messages, matchesAdapter, previewMessagesAdapter)
-        }
         /*
         val layout : ExpandableLayout = findViewById(R.id.expandable_layout)
         val renderer = MyRenderer()
