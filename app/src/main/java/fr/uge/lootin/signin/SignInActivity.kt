25658802923aa154560cdbf2a871d3a9b7fe6d83
LouisBillaut <louisbillaut2000@gmail.com>
2021-03-15 16:15:39 +0100

    feat: Login connected to back & jwt saved in shared preference

--- app/src/main/java/fr/uge/lootin/signin/SignInActivity.kt
+++ app/src/main/java/fr/uge/lootin/signin/SignInActivity.kt
@@ -1,31 +1,37 @@
 package fr.uge.lootin.signin
 
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import fr.uge.lootin.R
+import org.json.JSONObject
 
 
 class SignInActivity : AppCompatActivity() {
-    private var email: String = ""
+    private var username: String = ""
     private var password: String = ""
+    private var token: String = ""
 
     private fun checkParams(): Boolean {
-        email = findViewById<EditText>(R.id.EmailOnsignInPage).text.toString()
+        username = findViewById<EditText>(R.id.UsernameOnsignInPage).text.toString()
         password = findViewById<EditText>(R.id.PasswordOnsignInPage).text.toString()
-
-        //TODO
-
-        return true
-    }
-
-
-    private fun googleConnect(): Boolean {
-
-        //TODO
-
+        if (username == "" || password == "") {
+            Toast.makeText(
+                    applicationContext,
+                    applicationContext.getString(R.string.loginMessageError),
+                    Toast.LENGTH_SHORT
+            ).show()
+            return false
+        }
         return true
     }
 
@@ -33,15 +39,44 @@ class SignInActivity : AppCompatActivity() {
         //TODO
     }
 
+    private fun saveJWT(jwt: String) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = preferences.edit()
+        editor.putString("jwt", jwt)
+        editor.commit()
+        launchRegisterActivity()
+    }
+
+    private fun login(queue: RequestQueue) {
+        val url = "http://192.168.1.86:8080/login"
+        Log.i("test", "login request")
+        Log.i("test", "username=${this.username}, password=${this.password}")
+        val jsonObjectRequest = object : JsonObjectRequest(Method.POST, url, JSONObject("{\"username\":\"${this.username}\",\"password\":\"${this.password}\"}"),
+                Response.Listener { response -> saveJWT(response.getString("jwt")) },
+                Response.ErrorListener { error ->
+                    Log.i("test", "error while trying to login\n"
+                            + error.toString() + "\n"
+                            + "networkResponse " + error.networkResponse + "\n"
+                            + "localizedMessage " + error.localizedMessage + "\n"
+                            + "message " + error.message + "\n"
+                            + "cause " + error.cause + "\n"
+                            + "stackTrace " + error.stackTrace.toString())
+                }) {
+            override fun getHeaders(): Map<String, String>? {
+                val params: MutableMap<String, String> = HashMap()
+                params["Content-Type"] = "application/json"
+                return params
+            }
+        }
+        queue.add(jsonObjectRequest)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
-        //google button
-        findViewById<Button>(R.id.GoogleButtonOnsignInPage).setOnClickListener { googleConnect() }
-
+        val queue = Volley.newRequestQueue(this)
         //connection button
-        findViewById<Button>(R.id.ConnectionButtonOnsignInPage).setOnClickListener { checkParams() }
+        findViewById<Button>(R.id.ConnectionButtonOnsignInPage).setOnClickListener { if (checkParams()) login(queue) }
 
         //Register button
         findViewById<TextView>(R.id.RegisterLinkOnsignInPage).setOnClickListener { launchRegisterActivity() }
