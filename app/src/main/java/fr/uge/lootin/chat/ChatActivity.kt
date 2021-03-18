681ab6d675b82d9e65dfe38def31613b875e6f8e
JUILLARD-Thomas <thomasjuillard@orange.fr>
2021-03-18 16:58:18 +0100

    feat: add factory for create Message

--- app/src/main/java/fr/uge/lootin/chat/ChatActivity.kt
+++ app/src/main/java/fr/uge/lootin/chat/ChatActivity.kt
@@ -25,14 +25,11 @@ import org.json.JSONObject
 import java.net.URI
 import java.util.*
 
-const val TOKEN_VALUE = "fr.uge.lootin.TOKEN"
-const val MATCH_ID = "fr.uge.lootin.MATCHID"
+
 
 class ChatActivity : AppCompatActivity() {
     lateinit var recycler : RecyclerView
     lateinit var adapter: ChatAdapter
-    var match_id : Long = 0
-    var page : Int = 0
 
     override fun onResume() {
         super.onResume()
@@ -41,55 +38,6 @@ class ChatActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
     }
-
-
-
-
-
-
-    private fun getOldMessages(nb_matches: Int, page: Int){
-        /*TODO*/
-    }
-
-
-    private fun getnewMessages(nb_matches: Int, page: Int){
-        /*TODO*/
-    }
-
-    /**
-     * Send message
-
-    private fun postMessages(content: String) {
-        val url = "http://$localhost:8080/msg/newMessage"
-
-        val jsonObjectRequest = object : JsonObjectRequest(
-                Request.Method.POST, url, JSONObject("{\"text\": $content,\"matchId\":$match_id}"),
-                Response.Listener { response ->
-                    receiveText(response["message"] as String, response["id"] as Long, response["Timestamp"] as Date)
-                    Log.i("my_log", "Response: %s".format(response.toString()))
-                },
-                Response.ErrorListener { error ->
-                    Log.i("my_log", "error while trying to verify connexion\n"
-                            + error.toString() + "\n"
-                            + error.networkResponse + "\n"
-                            + error.localizedMessage + "\n"
-                            + error.message + "\n"
-                            + error.cause + "\n"
-                            + error.stackTrace.toString())
-                }
-        ) {
-            @Throws(AuthFailureError::class)
-            override fun getHeaders(): Map<String, String>? {
-                val params: MutableMap<String, String> = HashMap()
-                params["Authorization"] = "Bearer $token"
-
-                return params
-            }
-        }
-        queue.add(jsonObjectRequest)
-    }
-    */
-
 
 
     /**
@@ -101,75 +49,106 @@ class ChatActivity : AppCompatActivity() {
 
         recycler = findViewById(R.id.reclyclerChat)
         val token = intent.getStringExtra(TOKEN_VALUE).toString()
-        match_id = intent.getLongExtra(MATCH_ID, -1)
-
-
-
-        //connect(queue)
+        val matchId = intent.getLongExtra(MATCH_ID, -1)
 
         adapter = ChatAdapter(ArrayList())
         recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, true)
 
-        val restService = RestService(LOCALHOST, MATCH_ID, PAGE_SIZE, adapter )
+        val restService = RestService(LOCALHOST, matchId, PAGE_SIZE, adapter, token)
 
+        // Create scrollListener
         recycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
 
             override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
                 super.onScrollStateChanged(recyclerView, newState)
-                Log.i("stated", newState.toString())
+                Log.i(TAG, newState.toString())
                 if (!recyclerView.canScrollVertically(-1) && newState == RecyclerView.SCROLL_STATE_IDLE) {
-                    /*TODO load messages*/
-                    restService.getMessages()
-                    Log.d("my_log", "end");
+
+                //TODO heheh ----------restService.getMessages()
                 }
             }
         })
 
-        //getMessages()
-        findViewById<ImageButton>(R.id.imageButtonsendText).setOnClickListener { sendText() }
-        findViewById<ImageButton>(R.id.imageButtonPicture).setOnClickListener { sendPicture() }
-        findViewById<ImageButton>(R.id.imageButtoncamera).setOnClickListener { sendVocal() }
+        //TODO call getMessages() and look pages
 
+        // Send Text messages
+        findViewById<ImageButton>(R.id.imageButtonsendText).setOnClickListener {
+            val message : String = findViewById<TextView>(R.id.zoneText).text.toString()
+            if (message.isNotEmpty()) {
+                // TODO postMessages(message)
+            }
+        }
 
-    }
+        //TODO Send Picture messages (WS)
+        findViewById<ImageButton>(R.id.imageButtonPicture).setOnClickListener {
+            Toast.makeText(this, "Send picture is not implemented yet", Toast.LENGTH_LONG).show()
+        }
 
-    /**
-     * Send text message (call web socket
-     */
-    private fun sendText() {
-        val message : String = findViewById<TextView>(R.id.zoneText).text.toString()
-        if (message.isNotEmpty()) {
-            //postMessages(message)
+        //TODO Send Vocal messages (WS)
+        findViewById<ImageButton>(R.id.imageButtoncamera).setOnClickListener {
+            Toast.makeText(this, "Send vocal is not implemented yet", Toast.LENGTH_LONG).show()
         }
     }
 
+
     //TODO virer moi ça ...
     private fun receiveText(message: String, id: Long, date: Date){
-        adapter.pushFrontFirst(MessageItemUi(message, Color.WHITE, MessageItemUi.TYPE_MY_MESSAGE, id, date))
+
+        adapter.pushFrontFirst(MessageItemUi.factoryMessageItemUI(message, id, date, true))
         findViewById<TextView>(R.id.zoneText).text = ""
         recycler.scrollToPosition(0) //TODO ça c'est cool
     }
 
-    // TODO faire avec web socket
-    private fun sendPicture() {
-        Toast.makeText(this, "Send picture is not implemented yet", Toast.LENGTH_LONG).show()
-    }
-
-    // HAha on verra ça les bg
-    private fun sendVocal() {
-        Toast.makeText(this, "Send vocal is not implemented yet", Toast.LENGTH_LONG).show()
-    }
 
     companion object {
         private const val TAG = "MAINACTIVITY"
         const val LOGIN = "login"
         const val PASSCODE = "passcode"
 
+        const val TOKEN_VALUE = "fr.uge.lootin.TOKEN"
+        const val MATCH_ID = "fr.uge.lootin.MATCHID"
+
         const val LOCALHOST: String = "192.168.1.58"
         const val PORT:String = "8080"
         const val ENPOINT: String = "gs-guide-websocket"
-        const val PAGE_SIZE: String = "10S"
+
+        const val PAGE_SIZE: Long = 10
     }
 
 }
+
+
+/**
+ * Send message
+
+private fun postMessages(content: String) {
+val url = "http://$localhost:8080/msg/newMessage"
+
+val jsonObjectRequest = object : JsonObjectRequest(
+Request.Method.POST, url, JSONObject("{\"text\": $content,\"matchId\":$match_id}"),
+Response.Listener { response ->
+receiveText(response["message"] as String, response["id"] as Long, response["Timestamp"] as Date)
+Log.i("my_log", "Response: %s".format(response.toString()))
+},
+Response.ErrorListener { error ->
+Log.i("my_log", "error while trying to verify connexion\n"
++ error.toString() + "\n"
++ error.networkResponse + "\n"
++ error.localizedMessage + "\n"
++ error.message + "\n"
++ error.cause + "\n"
++ error.stackTrace.toString())
+}
+) {
+@Throws(AuthFailureError::class)
+override fun getHeaders(): Map<String, String>? {
+val params: MutableMap<String, String> = HashMap()
+params["Authorization"] = "Bearer $token"
+
+return params
+}
+}
+queue.add(jsonObjectRequest)
+}
+ */
